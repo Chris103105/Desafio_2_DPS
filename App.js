35d1d1a68f6804309cd85f1e0d2a33e84eae292a
Chris103105 <christopherjovel7@gmail.com>
2026-04-12 18:08:16 -0600

    Configurando el layout inicial y colores base. Aún sin funcionalidad.

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.contenedor}>
+      {/* Título de la App */}
+      <Text style={styles.tituloPrincipal}>Piezas</Text>
+      
+      {/* Botón inicial (Aún no tiene funcionalidad) */}
+      <TouchableOpacity style={styles.botonAdd}>
+        <Text style={styles.textoBotonAdd}>+ Agregar Pieza</Text>
+      </TouchableOpacity>
+
+      {/* Mensaje de estado vacío */}
+      <View style={styles.contenedorVacio}>
+        <Text style={styles.textoVacio}>No hay piezas, Agregue una</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// Estilos básicos iniciales
 const styles = StyleSheet.create({
-  container: {
+  contenedor: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F7F9F7', // Nuestro fondo relajante
+    paddingTop: 80, // Espacio para que no choque con la hora del celular
+    paddingHorizontal: 20,
   },
+  tituloPrincipal: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#000000',
+    textAlign: 'center',
+    marginBottom: 30,
+  },
+  botonAdd: {
+    backgroundColor: '#000742', // Verde salvia
+    padding: 15,
+    borderRadius: 12,
+    alignItems: 'center',
+  },
+  textoBotonAdd: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  contenedorVacio: {
+    alignItems: 'center',
+    marginTop: 80,
+  },
+  textoVacio: {
+    fontSize: 16,
+    color: '#102647',
+  }
 });
